5f0c3a8d63fa144baac10c1a6e0feb53640d4fce
tickstep <tickstep@outlook.com>
2020-09-12 21:33:17 +0800

    add AppDeleteFile api

---
+++ cloudpan/app_delete_file.go
@@ -0,0 +1,31 @@
+package cloudpan
+
+import (
+	"fmt"
+	"github.com/tickstep/cloudpan189-api/cloudpan/apierror"
+	"github.com/tickstep/cloudpan189-api/cloudpan/apiutil"
+	"github.com/tickstep/library-go/logger"
+	"strings"
+)
+
+func (p *PanClient) AppDeleteFile(fileIdList []string) (bool, *apierror.ApiError) {
+	fullUrl := &strings.Builder{}
+	fmt.Fprintf(fullUrl, "%s/batchDeleteFile.action?fileIdList=%s&%s",
+		API_URL, strings.Join(fileIdList, ";"), apiutil.PcClientInfoSuffixParam())
+	httpMethod := "POST"
+	dateOfGmt := apiutil.DateOfGmtStr()
+	appToken := p.appToken
+	headers := map[string]string {
+		"Date": dateOfGmt,
+		"SessionKey": appToken.SessionKey,
+		"Signature": apiutil.SignatureOfHmac(appToken.SessionSecret, appToken.SessionKey, httpMethod, fullUrl.String(), dateOfGmt),
+		"X-Request-ID": apiutil.XRequestId(),
+	}
+	logger.Verboseln("do request url: " + fullUrl.String())
+	_, err1 := p.client.Fetch(httpMethod, fullUrl.String(), nil, headers)
+	if err1 != nil {
+		logger.Verboseln("AppMkdir occurs error: ", err1.Error())
+		return false, apierror.NewApiErrorWithError(err1)
+	}
+	return true, nil
+}
