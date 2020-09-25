107c01a27f6060d787e219d2b2a6c55e1de29657
tickstep <tickstep@outlook.com>
2020-09-25 20:34:21 +0800

    add app file directory api

--- cloudpan/app_file_directory.go
+++ cloudpan/app_file_directory.go
@@ -6,7 +6,9 @@ import (
 	"github.com/tickstep/cloudpan189-api/cloudpan/apierror"
 	"github.com/tickstep/cloudpan189-api/cloudpan/apiutil"
 	"github.com/tickstep/library-go/logger"
+	"math"
 	"net/url"
+	"path"
 	"strings"
 )
 
@@ -62,14 +64,11 @@ type (
 
 	// AppFileListResult 文件列表响应值
 	AppFileListResult struct {
-		XMLName xml.Name `xml:"listFiles"`
-		LastRev string `xml:"lastRev"`
+		LastRev string
 		// 总数量
-		Count int `xml:"fileList>count"`
-		// 文件夹列表
-		FolderList AppFileList `xml:"fileList>folder"`
+		Count int
 		// 文件列表
-		FileList AppFileList `xml:"fileList>file"`
+		FileList AppFileList
 	}
 
 	// AppFileEntity 文件/文件夹信息
@@ -220,6 +219,99 @@ func NewAppFileListParam() *AppFileListParam {
 	}
 }
 
+func NewAppFileEntityForRootDir() *AppFileEntity {
+	return &AppFileEntity {
+		FileId: "-11",
+		IsFolder: true,
+		FileName: "/",
+		ParentId: "-11",
+	}
+}
+
+
+// TotalSize 获取目录下文件的总大小
+func (afl AppFileList) TotalSize() int64 {
+	var size int64
+	for k := range afl {
+		if afl[k] == nil {
+			continue
+		}
+
+		size += afl[k].FileSize
+	}
+	return size
+}
+
+
+// Count 获取文件总数和目录总数
+func (afl AppFileList) Count() (fileN, directoryN int64) {
+	for k := range afl {
+		if afl[k] == nil {
+			continue
+		}
+
+		if afl[k].IsFolder {
+			directoryN++
+		} else {
+			fileN++
+		}
+	}
+	return
+}
+
+func (f *AppFileEntity) String() string {
+	builder := &strings.Builder{}
+	builder.WriteString("文件ID: " + f.FileId + "\n")
+	builder.WriteString("文件名: " + f.FileName + "\n")
+	if f.IsFolder {
+		builder.WriteString("文件类型: 目录\n")
+	} else {
+		builder.WriteString("文件类型: 文件\n")
+	}
+	builder.WriteString("文件路径: " + f.Path + "\n")
+	return builder.String()
+}
+
+// AppGetAllFileList 获取指定目录下的所有文件列表
+func (p *PanClient) AppGetAllFileList(param *AppFileListParam) (*AppFileListResult, *apierror.ApiError)  {
+	internalParam := &AppFileListParam{
+		FamilyId: param.FamilyId,
+		FileId: param.FileId,
+		OrderBy: param.OrderBy,
+		OrderSort: param.OrderSort,
+		PageNum: 1,
+		PageSize: param.PageSize,
+	}
+	if internalParam.PageSize <= 0 {
+		internalParam.PageSize = 200
+	}
+
+	result := &AppFileListResult{}
+	fileResult, err := p.AppFileList(internalParam)
+	if err != nil {
+		return nil, err
+	}
+	result.LastRev = fileResult.LastRev
+	result.FileList = fileResult.FileList
+	result.Count = fileResult.Count
+
+	// more page?
+	if fileResult.Count > int(internalParam.PageSize) {
+		pageCount := int(math.Ceil(float64(fileResult.Count) / float64(internalParam.PageSize)))
+		for page := 2; page <= pageCount; page++ {
+			internalParam.PageNum = uint(page)
+			fileResult, err = p.AppFileList(internalParam)
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+			result.FileList = append(result.FileList, fileResult.FileList...)
+		}
+	}
+
+	return result, nil
+}
+
 // AppFileList 获取文件列表
 func (p *PanClient) AppFileList(param *AppFileListParam) (*AppFileListResult, *apierror.ApiError) {
 	fullUrl := &strings.Builder{}
@@ -236,6 +328,9 @@ func (p *PanClient) AppFileList(param *AppFileListParam) (*AppFileListResult, *a
 		sessionSecret = p.appToken.SessionSecret
 	} else {
 		// 家庭云
+		if param.FileId == "-11" {
+			param.FileId = ""
+		}
 		fmt.Fprintf(fullUrl, "%s/family/file/listFiles.action?folderId=%s&familyId=%d&fileType=0&iconOption=0&mediaAttr=0&orderBy=%d&descending=%t&pageNum=%d&pageSize=%d&%s",
 			API_URL,
 			param.FileId, param.FamilyId, param.OrderBy, param.OrderSort == OrderDesc, param.PageNum, param.PageSize,
@@ -267,22 +362,175 @@ func (p *PanClient) AppFileList(param *AppFileListParam) (*AppFileListResult, *a
 			}
 		}
 	}
-	item := &AppFileListResult{}
-	if err := xml.Unmarshal(respBody, item); err != nil {
+
+	type appFileListResultInternal struct {
+		//XMLName xml.Name `xml:"listFiles"`
+		LastRev string `xml:"lastRev"`
+		// 总数量
+		Count int `xml:"fileList>count"`
+		// 文件夹列表
+		FolderList AppFileList `xml:"fileList>folder"`
+		// 文件列表
+		FileList AppFileList `xml:"fileList>file"`
+	}
+	itemResult := &appFileListResultInternal{}
+	if err := xml.Unmarshal(respBody, itemResult); err != nil {
 		logger.Verboseln("AppFileList parse response failed")
 		return nil, apierror.NewApiErrorWithError(err)
 	}
 
-	if item.FolderList != nil && len(item.FolderList) > 0 {
-		for _,item := range item.FolderList {
+	result := &AppFileListResult{
+		LastRev: itemResult.LastRev,
+		Count: itemResult.Count,
+		FileList: AppFileList{},
+	}
+
+	if itemResult.FolderList != nil && len(itemResult.FolderList) > 0 {
+		for _,item := range itemResult.FolderList {
 			item.IsFolder = true
+
+			result.FileList = append(result.FileList, item)
 		}
 	}
-	if item.FileList != nil && len(item.FileList) > 0 {
-		for _,item := range item.FileList {
+	if itemResult.FileList != nil && len(itemResult.FileList) > 0 {
+		for _,item := range itemResult.FileList {
 			item.IsFolder = false
+
+			result.FileList = append(result.FileList, item)
 		}
 	}
 
-	return item, nil
+	return result, nil
+}
+
+// AppGetFilePathById 通过FileId获取文件的绝对路径
+func (p *PanClient) AppFilePathById(familyId int64, fileId string) (string, *apierror.ApiError) {
+	param := &AppGetFileInfoParam{
+		FamilyId: familyId,
+		FileId: fileId,
+	}
+
+	fullPath := ""
+	for {
+		fi,err := p.AppGetBasicFileInfo(param)
+		if err != nil {
+			return "", err
+		}
+
+		// 个人云支持
+		if fi.Path != "" {
+			return fi.Path, nil
+		}
+
+		if strings.Index(fi.FileId, "-") == 0 || strings.Index(fi.ParentId, "-") == 0 {
+			// root file id
+			fullPath = "/" + fullPath
+			break
+		}
+		if fullPath == "" {
+			fullPath = fi.FileName
+		} else {
+			fullPath = fi.FileName + "/" + fullPath
+		}
+
+		// next loop
+		param.FileId = fi.ParentId
+	}
+	return fullPath, nil
+}
+
+// AppFileInfoById 通过FileId获取文件详情
+func (p *PanClient) AppFileInfoById(familyId int64, fileId string) (fileInfo *AppFileEntity, error *apierror.ApiError) {
+	basicFileInfo, err := p.AppGetBasicFileInfo(&AppGetFileInfoParam{FamilyId: familyId, FileId: fileId})
+	if err != nil {
+		return nil, err
+	}
+
+	param := NewAppFileListParam()
+	param.FamilyId = familyId
+	param.FileId = basicFileInfo.ParentId
+	allFileInfo, err1 := p.AppGetAllFileList(param)
+	if err1 != nil {
+		return nil, err1
+	}
+
+	for _,item := range allFileInfo.FileList {
+		if item.FileId == fileId {
+			return item, nil
+		}
+	}
+	return nil, nil
+}
+
+// AppFileInfoByPath 通过路径获取文件详情，pathStr是绝对路径
+func (p *PanClient) AppFileInfoByPath(familyId int64, pathStr string) (fileInfo *AppFileEntity, error *apierror.ApiError) {
+	if pathStr == "" {
+		pathStr = "/"
+	}
+	//pathStr = path.Clean(pathStr)
+	if !path.IsAbs(pathStr) {
+		return nil, apierror.NewFailedApiError("pathStr必须是绝对路径")
+	}
+	if len(pathStr) > 1 {
+		pathStr = path.Clean(pathStr)
+	}
+
+	var pathSlice []string
+	if pathStr == "/" {
+		pathSlice = []string{""}
+	} else {
+		pathSlice = strings.Split(pathStr, PathSeparator)
+		if pathSlice[0] != "" {
+			return nil, apierror.NewFailedApiError("pathStr必须是绝对路径")
+		}
+	}
+	return p.getAppFileInfoByPath(familyId, 0, &pathSlice, nil)
+}
+
+func (p *PanClient) getAppFileInfoByPath(familyId int64, index int, pathSlice *[]string, parentFileInfo *AppFileEntity) (*AppFileEntity, *apierror.ApiError)  {
+	if parentFileInfo == nil {
+		// default root "/" entity
+		parentFileInfo = NewAppFileEntityForRootDir()
+		if index == 0 && len(*pathSlice) == 1 {
+			// root path "/"
+			return parentFileInfo, nil
+		}
+
+		return p.getAppFileInfoByPath(familyId, index + 1, pathSlice, parentFileInfo)
+	}
+
+	if index >= len(*pathSlice) {
+		return parentFileInfo, nil
+	}
+
+	searchPath := NewAppFileListParam()
+	searchPath.FileId = parentFileInfo.FileId
+	searchPath.FamilyId = familyId
+	fileResult, err := p.AppGetAllFileList(searchPath)
+	if err != nil {
+		return nil, err
+	}
+
+	if fileResult == nil || fileResult.FileList == nil || len(fileResult.FileList) == 0  {
+		return nil, apierror.NewApiError(apierror.ApiCodeFileNotFoundCode, "文件不存在")
+	}
+	for _, fileEntity := range fileResult.FileList {
+		if fileEntity.FileName == (*pathSlice)[index] {
+			fileEntity.ParentId = parentFileInfo.FileId
+			fileEntity.Path = getPath(index, pathSlice)
+			return p.getAppFileInfoByPath(familyId, index + 1, pathSlice, fileEntity)
+		}
+	}
+	return nil, apierror.NewApiError(apierror.ApiCodeFileNotFoundCode, "文件不存在")
+}
+
+func getPath(index int, pathSlice *[]string) string {
+	fullPath := ""
+	for idx, str := range *pathSlice {
+		if idx > index {
+			break
+		}
+		fullPath += "/" + str
+	}
+	return strings.ReplaceAll(fullPath, "//", "/")
 }
